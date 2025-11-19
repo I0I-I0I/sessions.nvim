70962f2c3f601f3c0c7fe0301802154bddeaeaca
I0I-I0I <azudjndj@gmail.com>
2025-11-20 02:24:46 +0300

    refactor: improve sorting

--- lua/telescope/_extensions/sessionizer/utils.lua
+++ lua/telescope/_extensions/sessionizer/utils.lua
@@ -1,9 +1,36 @@
+local commands_utils = require("sessions.commands._utils")
+
 local M = {}
+
+---@param a Session
+---@param b Session
+---@return boolean
+local function compare_sessions(a, b)
+    local pa, pb = commands_utils.is_pinned(a), commands_utils.is_pinned(b)
+    if pa ~= pb then
+        return pa
+    end
+
+    local a_has_custom_name = a.name ~= a.path
+    local b_has_custom_name = b.name ~= b.path
+    if a_has_custom_name ~= b_has_custom_name then
+        return a_has_custom_name
+    end
+
+    if a.last_used ~= b.last_used then
+        return a.last_used > b.last_used
+    end
+
+    if a.name ~= b.name then
+        return a.name < b.name
+    end
+
+    return a.path < b.path
+end
 
 ---@return Session[]
 function M.get_items()
     local session = require("sessions.session")
-    local commands_utils = require("sessions.commands._utils")
     local opts = require("sessions").get_opts()
 
     local all_sessions = session.get.all()
@@ -29,19 +56,7 @@ function M.get_items()
         ::continue::
     end
 
-    table.sort(items, function(a, b)
-        local pa, pb = commands_utils.is_pinned(a), commands_utils.is_pinned(b)
-        if pa ~= pb then
-            return pa
-        end
-        if a.last_used ~= b.last_used then
-            return a.last_used > b.last_used
-        end
-        if a.name ~= b.name then
-            return a.name < b.name
-        end
-        return a.path < b.path
-    end)
+    table.sort(items, compare_sessions)
 
     if current_session then
         table.insert(items, 1, current_session)
